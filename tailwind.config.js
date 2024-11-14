0362ded78ed5adc7ca85f67709fb84592b701e9e
freitas14.freitas@gmail.com <freitas14.freitas@gmail.com>
2024-11-14 11:41:23 -0300

    Arquivos base do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+              colors: {
+                terracota: '#da373d',
+                mostarda:  '#F7AE1A';
+                preto-medio: '#292625';
+                marron-escuro: '#3B2416';
+                marron-claro: '#A97744';
+                mude: '#F5E7DA';
+              }
+            }
+          }
+        }
+      </script>
+      },
+    },
+    plugins: [],
+  }
